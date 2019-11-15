22ef4c27963a6b52a09b87dbc88910b189991e35
tribe01 <oliver.tribess@bertelsmann.de>
2019-11-15 12:52:44 +0100

    refactoring

--- src/test/java/org/kie/client/springboot/samples/client/KIEClientApplicationTests.java
+++ src/test/java/org/kie/client/springboot/samples/client/KIEClientApplicationTests.java
@@ -23,7 +23,7 @@ public class KIEClientApplicationTests {
     try{
       kieClient.getKieServicesClient().getServerInfo().getResult();
     }catch(NoEndpointFoundException e){
-      throw new RuntimeException("Please start a KIE Server & KIE Workbench for the test: docker run -p 8080:8080 -p 8001:8001 -d --name jbpm-workbench jboss/jbpm-workbench-showcase:7.15.0.Final && docker run -p 8180:8080 -d --name kie-server --link jbpm-workbench:kie-wb jboss/kie-server-showcase:7.15.0.Final  && docker logs -f kie-server",e);
+      throw new RuntimeException("Please start a KIE Server & KIE Workbench for the test: docker run -p 8080:8080 -p 8001:8001 -d --name jbpm-workbench jboss/jbpm-workbench-showcase:7.15.0.Final && docker run -p 8180:8080 -d --name kie-server --link jbpm-workbench:kie-wb jboss/kie-server-showcase:7.15.0.Final && docker logs -f kie-server",e);
     }
     assertTrue("KIE Server is not able to handle BPMN", kieClient.getKieServicesClient().getServerInfo().getResult().getCapabilities().contains("BPM"));
   }
