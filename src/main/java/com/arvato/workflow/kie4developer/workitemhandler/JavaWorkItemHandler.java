a0f9edafc2e23d9a6dbdce6312cc48f8cacfab84
tribe01 <oliver.tribess@bertelsmann.de>
2020-06-26 07:06:27 +0200

    refactoring

--- src/main/java/com/arvato/workflow/kie4developer/workitemhandler/JavaWorkItemHandler.java
+++ src/main/java/com/arvato/workflow/kie4developer/workitemhandler/JavaWorkItemHandler.java
@@ -14,7 +14,8 @@ import org.slf4j.LoggerFactory;
 /**
  * A {@link IDeployableWorkItemHandler} implementation for executing Java classes via reflection. You must pass in the
  * variables <code>className</code> and <code>methodName</code> with optional <code>methodParameterType</code> and
- * <code>methodParameter</code> to invoke a custom class. The handler response with the map of results from the
+ * <code>methodParameter</code> to invoke a custom class. You can pass in optional a <code>errorHandingProcessId</code>
+ * to execute a exception handling process in case of an error. The handler response with the map of results from the
  * invocation call.
  *
  * @author TRIBE01
@@ -37,12 +38,13 @@ public class JavaWorkItemHandler implements IDeployableWorkItemHandler {
     String methodName = (String) getProcessVariableOrThrowException(workItem, "methodName");
     String parameterType = (String) workItem.getParameters().get("methodParameterType"); // optional
     Object parameter = workItem.getParameters().get("methodParameter"); // optional
-    String errorhandingprocessId = (String) workItem.getParameters().get("errorhandingprocessId"); // optional
+    String errorHandingProcessId = (String) workItem.getParameters().get("errorHandingProcessId"); // optional
 
     if (LOGGER.isDebugEnabled()) {
       // curl -X GET "http://{host}:{port}/kie-server/services/rest/server/containers/{containerid}/processes/instances/{processinstanceid}/variable/methodParameter" -H "accept: application/json"
       LOGGER
-          .debug("Try to execute Java Class {} with method {} with parameter type {} and parameter value {}.", className,
+          .debug("Try to execute Java Class {} with method {} with parameter type {} and parameter value {}.",
+              className,
               methodName, parameterType, parameter);
     } else {
       LOGGER.info("Try to execute Java Class {} with method {}.", className, methodName);
@@ -66,19 +68,19 @@ public class JavaWorkItemHandler implements IDeployableWorkItemHandler {
     } catch (NoClassDefFoundError e) {
       LOGGER.error("Class {} could not be instantiated. "
           + "Please verify that no action blocks the class instantiation via default constructor.", className, e);
-      handleException(errorhandingprocessId, workItem, manager, e);
+      handleException(workItem, manager, e, errorHandingProcessId);
     } catch (InvocationTargetException | RuntimeException e) {
       LOGGER.error(
           "Runtime Error while execute Java class {} with method {} with parameter type {} and parameter value {}.",
           className, methodName, parameterType, parameter, e);
-      handleException(errorhandingprocessId, workItem, manager, e);
+      handleException( workItem, manager, e, errorHandingProcessId);
     } catch (ReflectiveOperationException e) {
       LOGGER.error("Class {} or method {} could not be found or instantiated. "
           + "Please verify the existence of a default constructor and method.", className, methodName, e);
-      handleException(errorhandingprocessId, workItem, manager, e);
+      handleException(workItem, manager, e, errorHandingProcessId);
     } catch (Exception e) {
       LOGGER.error("Method {} in class {} throws an unexpected Exception.", methodName, className, e);
-      handleException(errorhandingprocessId, workItem, manager, e);
+      handleException(workItem, manager, e, errorHandingProcessId);
     }
   }
 
@@ -90,27 +92,26 @@ public class JavaWorkItemHandler implements IDeployableWorkItemHandler {
 
   /**
    * Handle execution errors
-   *
-   * @param errorhandingprocessId the process id of a separate error handling subprocesses
    * @param workItem              the workItem reference
    * @param manager               the manager reference
    * @param cause                 the exception cause
+   * @param errorHandingProcessId the process id of a separate error handling subprocesses to start (optional)
    * @see ProcessWorkItemHandlerException
    */
-  public void handleException(String errorhandingprocessId, WorkItem workItem, WorkItemManager manager,
-      Throwable cause) {
+  public void handleException(WorkItem workItem, WorkItemManager manager,
+      Throwable cause, String errorHandingProcessId) {
     if (retries < MAX_RETRIES) {
       try {
         // increase time on every retry: 11min, 121min, 1331min ...
         Thread.sleep(Double.doubleToLongBits(Math.pow(11, retries)) * 60 * 1000);
-      } catch (InterruptedException e) { }
+      } catch (InterruptedException e) {
+      }
       retries++;
       LOGGER.info("Retry execution #{}/{}.", retries, MAX_RETRIES);
       executeWorkItem(workItem, manager);
-    } else if (errorhandingprocessId != null) {
-      LOGGER.info("Starting error handling subprocess {}.", errorhandingprocessId);
-      throw new ProcessWorkItemHandlerException(errorhandingprocessId, HandlingStrategy.COMPLETE,
-          cause);  // error gets handled by separate error handling subprocesses
+    } else if (errorHandingProcessId != null) {
+      LOGGER.info("Starting error handling subprocess {}.", errorHandingProcessId);
+      throw new ProcessWorkItemHandlerException(errorHandingProcessId, HandlingStrategy.COMPLETE, cause);  // error gets handled by separate error handling subprocesses
     } else {
       throw new RuntimeException(cause);
     }
