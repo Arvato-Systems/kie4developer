3884fbe8a698ff98726b8d8abb856a575362f7d6
tribe01 <oliver.tribess@bertelsmann.de>
2020-09-10 06:40:55 +0200

    cleanup

--- src/main/java/com/arvato/workflow/kie4developer/common/impl/kjar/KJarBuilder.java
+++ src/main/java/com/arvato/workflow/kie4developer/common/impl/kjar/KJarBuilder.java
@@ -303,13 +303,13 @@ public class KJarBuilder {
    */
   private void addClassFileToDeployment(Class clazz, Map<String, File> classFilesToDeploy) throws IOException {
     String compiledClassesDir = clazz.getProtectionDomain().getCodeSource().getLocation().getFile();
-    System.out.println("(1) compiledClassesDir="+compiledClassesDir);
+    LOGGER.info("(1) compiledClassesDir="+compiledClassesDir);
     compiledClassesDir = compiledClassesDir.startsWith("file:") ? compiledClassesDir.substring(5) : compiledClassesDir;
-    System.out.println("(2) compiledClassesDir="+compiledClassesDir);
+    LOGGER.info("(2) compiledClassesDir="+compiledClassesDir);
     compiledClassesDir = (compiledClassesDir.startsWith("/") && compiledClassesDir.contains(":")) ? compiledClassesDir.substring(1) : compiledClassesDir;
-    System.out.println("(3) compiledClassesDir="+compiledClassesDir);
+    LOGGER.info("(3) compiledClassesDir="+compiledClassesDir);
     compiledClassesDir = URLDecoder.decode(compiledClassesDir, "UTF-8");
-    System.out.println("(4) compiledClassesDir="+compiledClassesDir);
+    LOGGER.info("(4) compiledClassesDir="+compiledClassesDir);
     Path directoryStreamPath = Paths.get(compiledClassesDir + clazz.getPackage().getName().replace(".", File.separator));
     directoryStreamPath = extractFileWhenIncludedInJar(directoryStreamPath);
 
